3d1d2866a2829b828c2bee5d13c7a34f9307bc01
Ankit Tanwar <tankitofficial01@gmail.com>
2023-11-30 22:00:10 +0530

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,63 @@
+'use client'
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
 import Link from 'next/link'
 import React from 'react'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
+
+
 
 export default function Main() {
+
+ 
+    const CustomLeftArrow = (props) => (
+        <div
+            {...props}
+            style={{ ...props.style, left: '10px', zIndex: 1,   }}
+        />
+    );
+
+    const CustomRightArrow = (props) => (
+        <div
+            {...props}
+            style={{ ...props.style, right: '10px', zIndex: 1 }}
+        />
+    );
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1, // Set to 1 to show only one image at a time
+        slidesToScroll: 1,
+        autoplay: true,       // Enable autoplay
+        autoplaySpeed: 2000,
+        prevArrow: <CustomLeftArrow />,
+        nextArrow: <CustomRightArrow />,
+        appendDots: (dots) => (
+            <div style={{ position: "absolute", bottom: "10px", width: "100%" }}>
+              <ul style={{ margin: "0", padding: "0", textAlign: "center" }}>{dots}</ul>
+            </div>
+          ),
+    };
+   
     return (
         <main className="main">
             <Container
                 sx={{
                     height: '100vh',
-                    
+
                     p: 0,
                     maxWidth: { xs: '100%', md: '1650px' },
-                    
+
                 }}
             >
 
-                <Grid container sx={{ overflowX: 'auto',  }}>
-                    <Grid container className='' gap={7}  sx={{
-                        mt:1,
+                <Grid container sx={{ overflowX: 'auto', }}>
+                    <Grid container className='' gap={7} sx={{
+                        mt: 1,
                         bgcolor: '#ffff',
                         minHeight: '14vh',
                         display: 'flex',
@@ -31,97 +71,112 @@ export default function Main() {
                         , flexWrap: 'nowrap',
                         '@media (max-width: 600px)': {
                             justifyContent: 'initial',
-                            fontSize:'14px',
-                            marginTop:'0px'
-                           
+                            fontSize: '14px',
+                            marginTop: '0px'
+
                         },
                     }}>
-                        <Grid item xs='auto'sx={{'@media (max-width: 600px)': {
-                            marginLeft:'45px',
-                            
-                           
-                        }}} >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000' ,textAlign:'center', display: 'block' }}>
+                        <Grid item xs='auto' sx={{
+                            '@media (max-width: 600px)': {
+                                marginLeft: '45px',
+
+
+                            }
+                        }} >
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/flap/64/64/image/29327f40e9c4d26b.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Grocery</span>
+                                    <span style={{ fontWeight: 500 }}>Grocery</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' variant="subtitle1" gutterBottom>
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/flap/64/64/image/22fddf3c7da4c4f4.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Mobile</span>
+                                    <span style={{ fontWeight: 500 }}>Mobile</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/fk-p-flap/64/64/image/0d75b34f7d8fbcb3.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Fashion</span>
+                                    <span style={{ fontWeight: 500 }}>Fashion</span>
                                 </div>
                             </a>
                         </Grid>
-                        <Grid item xs='auto' sx={{'@media (max-width: 600px)': {
-                            marginLeft:'10px',
-                            
-                           
-                        }}}>
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                        <Grid item xs='auto' sx={{
+                            '@media (max-width: 600px)': {
+                                marginLeft: '10px',
+
+
+                            }
+                        }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Electronic</span>
+                                    <span style={{ fontWeight: 500 }}>Electronic</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' className='' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign:'center', display: 'block'}}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' className='' src='https://rukminim1.flixcart.com/flap/64/64/image/ab7e2b022a4587dd.jpg?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Home & Furniture</span>
+                                    <span style={{ fontWeight: 500 }}>Home & Furniture</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/fk-p-flap/64/64/image/0139228b2f7eb413.jpg?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Appliances</span>
+                                    <span style={{ fontWeight: 500 }}>Appliances</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/flap/64/64/image/71050627a56b4693.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Travel</span>
+                                    <span style={{ fontWeight: 500 }}>Travel</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/flap/64/64/image/dff3f7adcf3a90c6.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Beauty, Toys & More</span>
+                                    <span style={{ fontWeight: 500 }}>Beauty, Toys & More</span>
                                 </div>
                             </a>
                         </Grid>
                         <Grid item xs='auto' >
-                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000',textAlign:'center', display: 'block' }}>
+                            <a href='#' className='' style={{ textDecoration: 'none', color: '#000', textAlign: 'center', display: 'block' }}>
                                 <img alt='gro_img' src='https://rukminim1.flixcart.com/fk-p-flap/128/128/image/05d708653beff580.png?q=100' style={{ width: '64px', height: '64px' }} />
                                 <div>
-                                    <span style={{fontWeight:500}}>Two Wheelers</span>
+                                    <span style={{ fontWeight: 500 }}>Two Wheelers</span>
                                 </div>
                             </a>
                         </Grid>
                     </Grid>
 
-
-
-
                 </Grid>
+
+
+                <Slider {...settings} style={{ marginTop: '10px' }}>
+                    <div>
+                        <img style={{ position: 'relative', width: '100%', display: 'flex', alignItems: 'flex-end' }} src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/6ae7e9b7ba9a7e13.jpg?q=20" alt="Slide 1" />
+                    </div>
+                    <div>
+                        <img style={{ position: 'relative', width: '100%', display: 'flex', alignItems: 'flex-end' }} src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/9d0e32c50dab0c5b.jpeg?q=20" alt="Slide 2" />
+                    </div>
+                    <div>
+                        <img style={{ position: 'relative', width: '100%', display: 'flex', alignItems: 'flex-end' }} src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/62fdd66868594b24.jpg?q=20" alt="Slide 3" />
+                    </div>
+                    {/* Add more slides as needed */}
+                </Slider>
 
             </Container>
         </main>
